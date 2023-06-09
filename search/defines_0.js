17f90acc1f4449ffd1a6dd6b570cccdafa6e375c
Github Action <ubpf@users.noreply.github.com>
2023-06-09 23:30:51 +0000

    Updated documentation
    Signed-off-by: Github Action <ubpf@users.noreply.github.com>

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['ubpf_5fmax_5finsts_44',['UBPF_MAX_INSTS',['../ubpf_8h.html#a7b029668bfe717c5d7b33fba3847a00f',1,'ubpf.h']]],
-  ['ubpf_5fstack_5fsize_45',['UBPF_STACK_SIZE',['../ubpf_8h.html#ab41a6986b0045cc2e5c84a75d08c7a12',1,'ubpf.h']]]
+  ['ubpf_5fmax_5fcall_5fdepth_45',['UBPF_MAX_CALL_DEPTH',['../ubpf_8h.html#a23e70b5f29f0296e69a72ec5cfc588d8',1,'ubpf.h']]],
+  ['ubpf_5fmax_5finsts_46',['UBPF_MAX_INSTS',['../ubpf_8h.html#a7b029668bfe717c5d7b33fba3847a00f',1,'ubpf.h']]],
+  ['ubpf_5fstack_5fsize_47',['UBPF_STACK_SIZE',['../ubpf_8h.html#ab41a6986b0045cc2e5c84a75d08c7a12',1,'ubpf.h']]]
 ];
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ubpf_5fbounds_5fcheck_41',['ubpf_bounds_check',['../ubpf_8h.html#a64bd2f640f675e3eeff48ea52583d699',1,'ubpf.h']]],
-  ['ubpf_5fdata_5frelocation_42',['ubpf_data_relocation',['../ubpf_8h.html#a70fb644d080e8448c1db17bb99858391',1,'ubpf.h']]],
-  ['ubpf_5fjit_5ffn_43',['ubpf_jit_fn',['../ubpf_8h.html#a6334115c4408267ec35087ade6128fa0',1,'ubpf.h']]]
+  ['ubpf_5fbounds_5fcheck_42',['ubpf_bounds_check',['../ubpf_8h.html#a64bd2f640f675e3eeff48ea52583d699',1,'ubpf.h']]],
+  ['ubpf_5fdata_5frelocation_43',['ubpf_data_relocation',['../ubpf_8h.html#a70fb644d080e8448c1db17bb99858391',1,'ubpf.h']]],
+  ['ubpf_5fjit_5ffn_44',['ubpf_jit_fn',['../ubpf_8h.html#a6334115c4408267ec35087ade6128fa0',1,'ubpf.h']]]
 ];
